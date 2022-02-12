8fdecda54fb2813cc0fc593c9d4900f39b4f4c72
Preeti Ladwa <preeti.ladwa@pace.edu>
2022-02-12 01:16:15 -0500

    first commit

---
+++ PreetiLadwa.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+<title>Assignment 1</title>
+</head>
+<body>
+
+<h1>This is my Assignment 1</h1>
+<h2>Function for sumAll</h1>
+    <?php
+        $a=array(5,15,25);
+        echo array_sum($a);
+    ?>
+
+
+</body>
+</html>
